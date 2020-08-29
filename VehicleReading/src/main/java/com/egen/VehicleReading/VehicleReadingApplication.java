930400df900b695322f5009d23aead680b997e6d
Sujata Patil <sujatapatil2010@gmail.com>
2020-08-29 09:26:35 -0500

    feat(vehicleReading): new module vehicleReading added

---
+++ VehicleReading/src/main/java/com/egen/VehicleReading/VehicleReadingApplication.java
@@ -0,0 +1,13 @@
+package com.egen.VehicleReading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleReadingApplication.class, args);
+	}
+
+}
---
+++ VehicleReading/src/test/java/com/egen/VehicleReading/VehicleReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egen.VehicleReading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
